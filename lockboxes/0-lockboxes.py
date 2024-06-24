f30d22eaf0c08ac820d03e7922dea2b105b962df
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-06-24 16:33:44 +0400

    1.3

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,26 +1,13 @@
 #!/usr/bin/python3
-from itertools import dropwhile
-
-
 def canUnlockAll(boxes):
-
-    keys = {0}
-    range_boxes = range(len(boxes))
-
-    while True:
-        not_found = set()
-
-        for i in range_boxes:
-            if i in keys:
-                for key in dropwhile(lambda k: k in keys, boxes[i]):
-                    keys.add(key)
-            else:
-                not_found.add(i)
-
-        if range_boxes == not_found:
-            return False
-
-        if not not_found:
-            return True
-
-        range_boxes = not_found
+    """ solve the problem of
+    loockboxes """
+    for key in range(1, len(boxes) - 1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True
