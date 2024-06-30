f615e5647ec0f4d8f7bd46130dcf8581ec4b72d8
Rasim Aghayev <rasimaqayev@gmail.com>
2024-07-01 00:29:20 +0400

    1.3

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -18,5 +18,21 @@ listint_t *insert_node(listint_t **head, int number)
 	if (new == NULL)
 		return (NULL);
 
+	new->n = number;
+
+	while (current && current->next && current->next->n < number)
+		current = current->next;
+
+	if ((current == NULL) || (number < current->n))
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = current->next;
+		current->next = new;
+	}
+
 	return (new);
 }
