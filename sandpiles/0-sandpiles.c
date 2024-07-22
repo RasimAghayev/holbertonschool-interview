af6612f199656ae9dc8b6fac121b5da1e00fe753
Rasim Aghayev <rasimaqayev@gmail.com>
2024-07-23 01:40:08 +0400

    code 1.8

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -13,28 +13,28 @@ int i = 0, j = 0;
 int flags[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
   /* sum grid1 and grid2 into grid1 */
-for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-    {
-      grid1[i][j] = grid1[i][j] + grid2[i][j];
-    }
-  }
-while (grid1_unstable(grid1, flags))
-  {
+for(i = 0; i < 3; i++)
+ {
+   for(j = 0; j < 3; j++)
+   {
+     grid1[i][j] = grid1[i][j] + grid2[i][j];
+   }
+ }
+while(grid1_unstable(grid1, flags))
+{
     /* print unstable grid before topple round */
     printf("=\n");
     print_grid(grid1);
     /* for every indicated flag, topple the cell */
-    for (i = 0; i < 3; i++)
+    for(i = 0; i < 3; i++)
     {
-      for (j = 0; j < 3; j++)
-      {
-        if (flags[i][j])
-          topple(grid1, i, j);
-      }
+     for(j = 0; j < 3; j++)
+     {
+       if(flags[i][j])
+         topple(grid1, i, j);
+     }
     }
-  }
+}
 }
 
 /**
@@ -50,19 +50,19 @@ int grid1_unstable(int grid1[3][3], int flags[3][3])
 {
 int check = 0, i = 0, j = 0;
 
-for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-    {
-      if (grid1[i][j] > 3)
-      {
-        check = 1;
-        flags[i][j] = 1;
-      }
-      else
-        flags[i][j] = 0;
-    }
-  }
+for(i = 0; i < 3; i++)
+ {
+   for(j = 0; j < 3; j++)
+   {
+     if(grid1[i][j] > 3)
+     {
+       check = 1;
+       flags[i][j] = 1;
+     }
+     else
+       flags[i][j] = 0;
+   }
+ }
 return (check);
 }
 
@@ -77,14 +77,14 @@ return (check);
 void topple(int grid1[3][3], int i, int j)
 {
 grid1[i][j] -= 4;
-if (i - 1 >= 0)
-    grid1[i - 1][j] += 1;
-if (i + 1 < 3)
-    grid1[i + 1][j] += 1;
-if (j - 1 >= 0)
+if(i - 1 >= 0)
+   grid1[i - 1][j] += 1;
+if(i + 1 < 3)
+   grid1[i + 1][j] += 1;
+if(j - 1 >= 0)
     grid1[i][j - 1] += 1;
-if (j + 1 < 3)
-    grid1[i][j + 1] += 1;
+if(j + 1 < 3)
+   grid1[i][j + 1] += 1;
 }
 
 /**
@@ -97,14 +97,14 @@ static void print_grid(int grid[3][3])
 {
 int i, j;
 
-for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-    {
-      if (j)
-        printf(" ");
-      printf("%d", grid[i][j]);
-    }
-    printf("\n");
-  }
+for(i = 0; i < 3; i++)
+ {
+   for(j = 0; j < 3; j++)
+   {
+     if(j)
+       printf(" ");
+     printf("%d", grid[i][j]);
+   }
+   printf("\n");
+ }
 }
