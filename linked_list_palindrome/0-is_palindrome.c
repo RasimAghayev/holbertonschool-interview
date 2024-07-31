822b152970a75bce625a307f795e7aa01610df7c
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-07-31 16:37:16 +0400

    0. Linked list palindrome

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: double pointer to the first node of a singly linked list
+ *
+ * Return: 0 if not palindrome, 1 if palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL)
+		return (0);
+	if (*head == NULL)
+		return (1);
+	return (palindrome_check(head, *head));
+}
+
+/**
+ * palindrome_check - checks is list is a palindrome recursively
+ * @head: double pointer to beginning of list
+ * @mover: single pointer to list to move to end
+ *
+ * Return: 0 if not palindrome, 1 if palindrome
+ */
+int palindrome_check(listint_t **head, listint_t *mover)
+{
+	if (mover->next)
+	{
+		if (palindrome_check(head, mover->next))
+			(*head) = (*head)->next;
+		else
+			return (0);
+	}
+	if ((*head)->n == mover->n)
+		return (1);
+	return (0);
+}
