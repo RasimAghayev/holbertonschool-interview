73a38e71413c8e0a977515eed9aeba4939386b52
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-08-19 09:27:33 +0000

    0. Palindrome unsigned integer

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "palindrome.h"
+
+int is_palindrome(unsigned long n) {
+  unsigned long reversed = 0, original = n, remainder;
+
+  while (n != 0) {
+    remainder = n % 10;
+    reversed = reversed * 10 + remainder;
+    n /= 10;
+  }
+
+  if (original == reversed)
+    return (1);
+  else
+    return (0);
+}
