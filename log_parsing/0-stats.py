79a31fbd0371bb2be17cb53766981fca88f2b946
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-09-01 21:45:13 +0000

    log_pars

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,59 +1,60 @@
 #!/usr/bin/python3
 """
-Script that reads stdin line by line and computes metrics:
-- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
-                code> <file size>
-- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
-    statistics from the beginning:
-Example:
-    File size: 5213
-    200: 2
-    401: 1
-    403: 2
-    404: 1
-    405: 1
-    500: 3
+Script to parse logs and compute metrics.
 """
+
 import sys
-stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
-        "403": 0, "404": 0, "405": 0, "500": 0}
-summ = 0
 
 
-def prn_stats(): # return void
+def print_stats(total_size, status_counts):
     """
-    Function that print stats about log
+    Print the accumulated metrics.
     """
-    global summ
+    print(f"File size: {total_size}")
+    for code in sorted(status_counts.keys()):
+        if status_counts[code] > 0:
+            print(f"{code}: {status_counts[code]}")
 
-    print('File size: {}'.format(summ))
-    stcdor = sorted(stcd.keys())
-    for each in stcdor:
-        if stcd[each] > 0:
-            print('{}: {}'.format(each, stcd[each]))
+
+def main():
+    total_size = 0
+    status_counts = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    }
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            parts = line.split()
+            if len(parts) >= 7:
+                try:
+                    status_code = int(parts[-2])
+                    file_size = int(parts[-1])
+
+                    if status_code in status_counts:
+                        status_counts[status_code] += 1
+                    total_size += file_size
+                except ValueError:
+                    continue
+
+                line_count += 1
+
+                if line_count % 10 == 0:
+                    print_stats(total_size, status_counts)
+
+    except KeyboardInterrupt:
+        print_stats(total_size, status_counts)
+        raise
+
+    print_stats(total_size, status_counts)
 
 
 if __name__ == "__main__":
-    cnt = 0
-    try:
-        """ Iter the standar input """
-        for data in sys.stdin:
-            try:
-                fact = data.split(' ')
-                """ If there is a status code """
-                if fact[-2] in stcd:
-                    stcd[fact[-2]] += 1
-                """ If there is a lenght """
-                summ += int(fact[-1])
-            except:
-                pass
-            """ Printing control """
-            cnt += 1
-            if cnt == 10:
-                prn_stats()
-                cnt = 0
-    except KeyboardInterrupt:
-        prn_stats()
-        raise
-    else:
-        prn_stats()
+    main()
