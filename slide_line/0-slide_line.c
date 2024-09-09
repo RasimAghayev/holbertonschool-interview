24fe5453f2c0690d1b3bc6f4fef167ab67a7f2eb
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-09-09 17:50:49 +0000

    slide_line

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -2,36 +2,47 @@
 
 static void merge_left(int *line, size_t size) {
   size_t i, j;
-  for (i = 0; i < size && line[i]; i++) {
-    for (j = i + 1; j < size; j++) {
-      if (line[j] == 0)
-        continue;
-      if (line[i] == line[j]) {
-        line[i] *= 2;
-        line[j] = 0;
+
+  for (i = 0; i < size; i++) {
+    if (line[i] != 0) {
+      for (j = i + 1; j < size; j++) {
+        if (line[j] == 0) {
+          continue;
+        }
+        if (line[i] == line[j]) {
+          line[i] *= 2;
+          line[j] = 0;
+          break; // Move to the next element
+        }
+        break; // Stop if the numbers are not the same
       }
-      break;
     }
   }
 }
 
 static void merge_right(int *line, size_t size) {
   size_t i, j;
-  for (i = size - 1; i < size && line[i]; i--) {
-    for (j = i - 1; j < size; j--) {
-      if (line[j] == 0)
-        continue;
-      if (line[i] == line[j]) {
-        line[i] *= 2;
-        line[j] = 0;
+
+  for (i = size - 1; i < size; i--) {
+    if (line[i] != 0) {
+      for (j = i - 1; (int)j >= 0; j--) {
+        if (line[j] == 0) {
+          continue;
+        }
+        if (line[i] == line[j]) {
+          line[i] *= 2;
+          line[j] = 0;
+          break; // Move to the next element
+        }
+        break; // Stop if the numbers are not the same
       }
-      break;
     }
   }
 }
 
 static void slide_left(int *line, size_t size) {
   size_t i, j = 0;
+
   for (i = 0; i < size; i++) {
     if (line[i] != 0) {
       line[j++] = line[i];
@@ -43,13 +54,14 @@ static void slide_left(int *line, size_t size) {
 }
 
 static void slide_right(int *line, size_t size) {
-  int i, j = size - 1;
-  for (i = size - 1; i >= 0; i--) {
+  size_t i, j = size - 1;
+
+  for (i = size - 1; i < size; i--) {
     if (line[i] != 0) {
       line[j--] = line[i];
     }
   }
-  for (; j >= 0; j--) {
+  for (; j < size; j++) {
     line[j] = 0;
   }
 }
@@ -60,9 +72,11 @@ int slide_line(int *line, size_t size, int direction) {
   }
 
   if (direction == SLIDE_LEFT) {
+    slide_left(line, size);
     merge_left(line, size);
     slide_left(line, size);
   } else {
+    slide_right(line, size);
     merge_right(line, size);
     slide_right(line, size);
   }
