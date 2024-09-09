387ee7026aa946ec669f8f87dc824499cca15a22
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-09-09 17:48:49 +0000

    slide_line

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -2,38 +2,30 @@
 
 static void merge_left(int *line, size_t size) {
   size_t i, j;
-  for (i = 0; i < size; i++) {
-    if (line[i] != 0) {
-      for (j = i + 1; j < size; j++) {
-        if (line[j] == 0) {
-          continue;
-        }
-        if (line[i] == line[j]) {
-          line[i] *= 2;
-          line[j] = 0;
-          break; // Move to next element after merging
-        }
-        break;
+  for (i = 0; i < size && line[i]; i++) {
+    for (j = i + 1; j < size; j++) {
+      if (line[j] == 0)
+        continue;
+      if (line[i] == line[j]) {
+        line[i] *= 2;
+        line[j] = 0;
       }
+      break;
     }
   }
 }
 
 static void merge_right(int *line, size_t size) {
   size_t i, j;
-  for (i = size - 1; i < size; i--) {
-    if (line[i] != 0) {
-      for (j = i - 1; (int)j >= 0; j--) {
-        if (line[j] == 0) {
-          continue;
-        }
-        if (line[i] == line[j]) {
-          line[i] *= 2;
-          line[j] = 0;
-          break; // Move to next element after merging
-        }
-        break;
+  for (i = size - 1; i < size && line[i]; i--) {
+    for (j = i - 1; j < size; j--) {
+      if (line[j] == 0)
+        continue;
+      if (line[i] == line[j]) {
+        line[i] *= 2;
+        line[j] = 0;
       }
+      break;
     }
   }
 }
@@ -51,13 +43,13 @@ static void slide_left(int *line, size_t size) {
 }
 
 static void slide_right(int *line, size_t size) {
-  size_t i, j = size - 1;
-  for (i = size - 1; i < size; i--) {
+  int i, j = size - 1;
+  for (i = size - 1; i >= 0; i--) {
     if (line[i] != 0) {
       line[j--] = line[i];
     }
   }
-  for (; j < size; j++) {
+  for (; j >= 0; j--) {
     line[j] = 0;
   }
 }
