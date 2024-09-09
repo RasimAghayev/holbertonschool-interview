61eafc25da92bf078649c42359c4a2e286b13b39
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-09-09 17:16:03 +0000

    slide_line

---
+++ slide_line/0-slide_line.c
@@ -0,0 +1,55 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - Slide and merge an array of integers to the left or right.
+ * @line: Pointer to the array of integers.
+ * @size: Number of elements in the array.
+ * @direction: Direction to slide (SLIDE_LEFT or SLIDE_RIGHT).
+ *
+ * Return: 1 on success, 0 on failure.
+ */
+int slide_line(int *line, size_t size, int direction) {
+  size_t i, j;
+  int temp[size];
+
+  if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
+    return (0);
+
+  /* Initialize temp array */
+  for (i = 0; i < size; i++)
+    temp[i] = 0;
+
+  if (direction == SLIDE_LEFT) {
+    j = 0;
+    for (i = 0; i < size; i++) {
+      if (line[i] != 0) {
+        if (temp[j] == line[i]) {
+          temp[j - 1] *= 2;
+          temp[j] = 0;
+        } else {
+          temp[j] = line[i];
+          j++;
+        }
+      }
+    }
+  } else if (direction == SLIDE_RIGHT) {
+    j = size - 1;
+    for (i = (int)(size - 1); i >= 0; i--) {
+      if (line[i] != 0) {
+        if (temp[j] == line[i]) {
+          temp[j + 1] *= 2;
+          temp[j] = 0;
+        } else {
+          temp[j] = line[i];
+          j--;
+        }
+      }
+    }
+  }
+
+  /* Copy the result to the original line array */
+  for (i = 0; i < size; i++)
+    line[i] = temp[i];
+
+  return (1);
+}
