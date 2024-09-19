c3dd9a4a139fb9ac01e575f29d137bf37e8c99a1
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-09-19 11:46:52 +0400

    Update 0-menger.c

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -11,14 +11,14 @@
  */
 int is_empty(int x, int y)
 {
-    while (x > 0 || y > 0)
-    {
-        if (x % 3 == 1 && y % 3 == 1)
-            return (1);
-        x /= 3;
-        y /= 3;
-    }
-    return (0);
+while (x > 0 || y > 0)
+{
+if (x % 3 == 1 && y % 3 == 1)
+ return (1);
+x /= 3;
+y /= 3;
+}
+return (0);
 }
 
 /**
@@ -27,22 +27,22 @@ int is_empty(int x, int y)
  */
 void menger(int level)
 {
-    int size, x, y;
+int size, x, y;
 
-    if (level < 0)
-        return;
+if (level < 0)
+return;
 
-    size = pow(3, level);
+size = pow(3, level);
 
-    for (y = 0; y < size; y++)
-    {
-        for (x = 0; x < size; x++)
-        {
-            if (is_empty(x, y))
-                printf(" ");
-            else
-                printf("#");
-        }
-        printf("\n");
-    }
+for (y = 0; y < size; y++)
+{
+for (x = 0; x < size; x++)
+{
+if (is_empty(x, y))
+printf(" ");
+else
+printf("#");
+}
+printf("\n");
+}
 }
